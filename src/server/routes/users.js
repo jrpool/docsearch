03a236aeb86d4ccaa28b0a703f955aee8dbc67d5
Jonathan Pool <pool@stulta.com>
2017-08-22 11:10:46 -0700

    Added password hashing on signup.

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -3,6 +3,13 @@ const {renderError} = require('../utils')
 
 const router = require('express').Router()
 
+const bcrypt = require('bcrypt');
+
+const hash_password = password => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+};
+
 router.get('/login', (request, response) => {
   response.render('login')
 })
@@ -35,10 +42,12 @@ router.post('/signup', (request, response, next) => {
     response.send('Your passwords are not identical!')
     return
   }
+  request.body.password1 = hash_password(request.body.password1);
+  request.body.password2 = '';
   DbUsers.checkUser(request.body)
   .then(user => {
     if (user !== null) {
-      response.send('You are already registered!')
+      response.send('Somebody with that username is already registered!')
       return ''
     }
     else {

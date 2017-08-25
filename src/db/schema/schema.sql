0162688735553cd9c8ee01374d7666ba314bc03c
Jonathan Pool <pool@stulta.com>
2017-08-25 10:39:20 -0700

    Added missing primary-key constraints to contact and member id columns.

--- src/db/schema/schema.sql
+++ src/db/schema/schema.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS contact, member, session;
 
 CREATE TABLE contact (
-  id serial,
+  id serial primary key,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
 );
 CREATE TABLE member (
-  id serial,
+  id serial primary key,
   username varchar(255) NOT NULL UNIQUE,
   hashed_password varchar(255) NOT NULL,
   admin boolean default false not null
